ff9a457cade3c7d913639377a37c6dd49f0098aa
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-01-06 18:47:18 +0100

    first add

---
+++ CustomerAppEntity/Customer.cs
@@ -0,0 +1,10 @@
+namespace CustomerAppEntity
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+}
--- CustomerAppUI/Program.cs
+++ CustomerAppUI/Program.cs
@@ -4,6 +4,7 @@ namespace CustomerAppUI
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
